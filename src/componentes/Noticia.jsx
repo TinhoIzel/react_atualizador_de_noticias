4891230fc69a582851deabd7de65386dbe6ebe25
TinhoIzel <tinhoizel@gmail.com>
2024-09-03 19:26:57 -0300

    mudei a função pra fora do useEffect

--- src/componentes/Noticia.jsx
+++ src/componentes/Noticia.jsx
@@ -1,57 +1,54 @@
 import { useState, useEffect } from "react";
 
 function Noticia() {
-
   // hook das noticias
   const [noticia, setNoticia] = useState(null);
 
   // pra atualizar noticias individualmente
   // não sei se tinha outra forma, mas funcionou!
-  const [atualizar,Atualizador] = useState(null)
+  const [atualizar, Atualizador] = useState(null);
 
 
-  
+
+  // função de requisição das noticias
+  const mudarNoticia = async () => {
+    // pra deixar aleatorio (só pra tentar não repetir)
+    const num = Math.ceil(Math.random() * 10);
+    console.log(num);
+
+    // a magia
+    const response = await fetch(
+      `https://jsonplaceholder.typicode.com/users/${num}`
+    );
+    const dadosNoticia = await response.json();
+    setNoticia(dadosNoticia);
+  };
+
+
+
   // useEffect que dá request pras "noticias"
   useEffect(() => {
-
-    // função de requisição das noticias
-    const mudarNoticia = async () => {
-
-      // pra deixar aleatorio (só pra tentar não repetir)
-      const num = Math.ceil(Math.random() * 10);
-      console.log(num);
-
-      // a magia
-      const response = await fetch(`https://jsonplaceholder.typicode.com/users/${num}`);
-      const dadosNoticia = await response.json();
-      setNoticia(dadosNoticia);
-
-    };
-
     // timer pra mudar as noticias a cada 30 segundos
     const timerId = setInterval(() => {
-        mudarNoticia();
+      mudarNoticia();
     }, 10000);
-
 
     // sempre que desmontar: resetar
     // as noticias e o timer
     // (ou resetando no inicio)
-    return() => {
-        setNoticia(null)
-        clearInterval(timerId)
-        mudarNoticia()
-    }
+    return () => {
+      setNoticia(null);
+      clearInterval(timerId);
+      mudarNoticia();
+    };
   }, [atualizar]);
 
 
-
+  
   // atualizando noticias individualmente
   const atualizarNoticia = () => {
-    Atualizador(prev => prev + 1)
-  }
-
-
+    Atualizador((prev) => prev + 1);
+  };
 
   return (
     <div>
